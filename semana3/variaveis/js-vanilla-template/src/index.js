9f1f2b356b71e0a35f399f65e82559bb06122786
Wellington de Brito <wellingtondebriito@gmail.com>
2020-09-28 14:53:31 -0300

    exercicio de variavel

---
+++ semana3/variaveis/js-vanilla-template/src/index.js
@@ -0,0 +1,79 @@
+// EXERCÍCIO 1
+
+/* Primeiro o console iŕa imprimir o valor da variável b que é 10, logo em seguida 
+houve uma troca de valor da variável b para 5, abaixo será impresso no console os valores de a e b
+que são 10 e 5 */
+
+// EXERCÍCIO 2
+
+// Será impresso no console os valores 10, 20 e a 
+
+
+// EXERCÍCIO DE 1 DE ESCRITA
+
+/* let nome 
+let idade 
+
+console.log(typeof (nome))
+console.log(typeof(idade)) */
+
+
+
+/* Foi impresso o tipo undefined, pois as variáveis
+não possumem valores informados */
+
+
+//let nome = prompt('Qual é o seu nome?')
+//let idade = prompt("Sua idade?")
+
+//console.log(nome)
+//console.log(idade)
+
+//console.log(typeof(nome))
+//console.log(typeof(idade))
+
+/* Notei que ele agora me informou que o tipo do valor da variável é uma string, pois foram
+dados inseridos pelo usuário*/
+
+//console.log("Olá", nome, "você tem", idade, "anos")
+
+// EXERCICIO 2 DE ESCRITA
+
+//let nome = prompt ("Qual o seu nome?")
+//let idade = prompt ("Sua idade?")
+//let filmeFavorito = prompt ("Seu filme favorito?")
+//let comidaFavorita = prompt ("Qual a sua comida favorita?")
+//let corFavorita = prompt ("Sua cor favorita?")
+
+/*console.log(nome)
+console.log(idade)
+console.log(filmeFavorito)
+console.log(comidaFavorita)
+console.log(corFavorita)*/
+
+/*EXERCICIO 3 DE ESCRITA */
+
+/*let comidaFavorita = ["Pizza", "Lasanha", "Carne seca com aimpim", "Estrogonofe", "Feijoada"]
+
+console.log(comidaFavorita)
+
+console.log("Essas são minhas comidas preferidas:")
+console.log(comidaFavorita[0])
+console.log(comidaFavorita[1])
+console.log(comidaFavorita[2])
+console.log(comidaFavorita[3])
+console.log(comidaFavorita[4])
+
+comidaFavorita[1] = prompt ("Sua comida predileta?")
+
+console.log(comidaFavorita) */
+
+// EXERCICIO 4 DE ESCRITA
+
+let minhasPerguntas = ["Você gosta de esportes?", "Gosta de comida japonesa?", "Hoje é segunda?"]
+let respostas = [true, false]
+
+console.log(minhasPerguntas[0], respostas[0])
+console.log(minhasPerguntas[1], respostas[1])
+console.log(minhasPerguntas[2], respostas[0])
+
