bbe2595c01e4cb348e589f46fdb10c6079302889
Wellington de Brito <wellingtondebriito@gmail.com>
2020-09-29 16:40:11 -0300

    projeto de operadores

---
+++ semana3/operadores/js-vanilla-template/src/index.js
@@ -0,0 +1,147 @@
+// EXERCÍCIO 1 DE INTERPRETAÇÃO DE CÓDIGOS
+
+// a. false
+// b. false
+// c. true
+// e. bolleans
+
+// EXERCÍCIO 2 DE INTERPRETAÇÃO DE CÓDIGOS
+
+// a. undefined
+// b. null
+// c. 11
+// d. 3
+// e. [3,19,4,5,6,7,8,9,10,11,12,13]
+// f. 9
+
+
+
+
+// EXERCÍCIO 1 DE ESCRITA DE CÓDIGO
+
+/*let suaIdade = prompt ("Qual a sua idade?")
+let idadeDoAmigo = prompt ("A idade do seu amigo ou amiga?")
+
+let maiorQue =  Number(suaIdade) > Number(idadeDoAmigo)
+
+console.log("Sua idade é maior que a do seu amigo?", maiorQue)
+
+const diferenca = suaIdade - idadeDoAmigo
+
+console.log("A diferença de idade é:", diferenca)*/
+
+
+// EXERCÍCIO 2 DE ESCRITA DE CÓDIGO
+
+/*let numeroPar = prompt("Digite um numero par")
+numeroPar = Number(numeroPar)
+
+console.log(numeroPar % 2)*/
+
+//Foi notado que todo o valor deu zero, pois todos os numeros pares é divisivel por 2
+
+// Todo o resultado do resto da divisão da o valor 1
+
+
+//EXERCÍCIO 3 DE ESCRITA DE CÓDIGO
+
+/*let listaDeTarefas = []
+let tarefa1 = prompt ("Qual a sua primeira tarefa do dia")
+let tarefa2 = prompt("Qual a segunda tarefa?")
+let tarefa3 = prompt ("Qual a terceira tarefa?")
+
+listaDeTarefas.push(tarefa1)
+listaDeTarefas.push(tarefa2)
+listaDeTarefas.push(tarefa3)
+
+//console.log(listaDeTarefas)
+
+let excluirTarefa = prompt ("Qual tarefa deseja exclui 0,1 ou 2?")
+console.log(excluirTarefa)
+listaDeTarefas.splice(excluirTarefa,1)
+console.log(listaDeTarefas)*/
+
+// EXERCÍCIO 4 DE ESCRITA DE CÓDIGO
+
+/*let nomeDoUsuario = prompt ("Insira o seu nome")
+let emailDoUsuario = prompt ("Insira o seu e-mail")
+let mensagem = alert ("O e-mail " + emailDoUsuario + " foi cadastrado com sucesso. Seja bem-vinda(a)," + nomeDoUsuario)*/
+
+
+
+// DESAFIOS
+
+// DESAFIO 1
+
+// a
+
+/*let temperatura1 = 77
+let resultado = (temperatura1 - 32)*5/9 + 273.15*/
+
+//console.log(resultado)
+
+// b
+
+/*let celsius = 80
+let resultado = (celsius)*9/5 + 32
+
+console.log(resultado)*/
+
+// c
+
+//let temperatura1 = 30
+//let temperatura2 = (temperatura1) * 9/5 + 32
+//let temperatura3 = (temperatura1) + 273.15
+//let resultado1 = temperatura2 + "F"
+//let resultado2 = temperatura3 + "K"
+
+//console.log (resultado1, resultado2)
+
+// d
+
+//let valor = prompt ("Digite uma temperatura em celsius para que seja convertido!")
+//let temperatura4 = Number(valor)
+//let resultado3 = (temperatura4) * 9/5 +32
+//let resultado4 = (temperatura4) + 273.15
+
+//console.log(resultado3 + "F " +resultado4 + "K")
+
+// DESAFIO 2
+
+// a.
+
+/*let energia = 280
+let valor = 0.05
+
+let resultado = energia * valor
+console.log(resultado)*/
+
+// b.
+
+/*let energia = 280
+let valor = 0.05
+let desconto = 0.05 * 15/100
+let valorTotal = energia * valor - desconto
+
+console.log(valorTotal)*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
