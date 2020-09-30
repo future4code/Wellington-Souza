a2211b361a8ef49d9eb473ddb83c14c28dfc82fa
Wellington de Brito <wellingtondebriito@gmail.com>
2020-09-30 16:18:06 -0300

    atividade de condicionais

---
+++ semana3/condicionais/src/index.js
@@ -0,0 +1,114 @@
+// EXERCICIO 1 DE INTERPRETAÇÃO DE CÓDIGO
+
+/* O teste realiza a operação do resto do divisor de 2,
+caso o valor do resto seja igual a 0 passou no teste, caso 
+ao contrario não passa no teste. */
+
+// Para passar nos testes o número tem que ser par.
+
+// Para não passar no teste os números digitados serão impares.
+
+
+// EXERCICIO 2 DE INTERPRETAÇÃO DE CÓDIGO
+
+/* 2.a O código imprime no console o preço das frutas escolhidas 
+pelo usuário */
+
+// 2.b O preço da fruta é R$ 2.25
+
+/* 2.c break serve para interromper o código caso o valor digitado
+pelo usuário for true, então se for retirado o código irá prosseguir
+e o valor impresso no console será 5 e não 5.5. */
+
+// EXERCICIO 3 DE INTERPRETAÇÃO DE CÓDIGO
+
+// 3.a Transforma uma string em numero
+
+/* 3.b Será impresso "Esse número passou no teste", se caso digite -10
+o código não irá prosseguir.
+
+/* 3.c O erro siginifica que a let mensagem pertence ao bloco do
+código if e que a mesma não está definida fora deste bloco.*/
+
+// EXERCÍCIO 4
+
+/*let idade = Number(prompt("Digite a sua idade: "))
+
+if (idade >= 18){
+    console.log("Você pode dirigir!")
+} else{
+    console.log("Você não pode dirigir!")
+}
+*/
+
+// EXERCÍCIO 5
+
+/*let turno = prompt ("Digite o turno que você estuda M(matutino), V(vespertino) e N(noturno)")
+
+if(turno === "M"){
+    console.log("Bom dia!")
+
+} else if (turno === "V"){
+    console.log("Boa tarde")
+} else {
+    console.log("Boa noite")
+}
+*/
+
+// EXERCÍCIO 6
+
+/*let turno = prompt ("Digite o turno que você estuda M(matutino), V(vespertino) e N(noturno)")
+
+switch (turno){
+    case "M":
+        console.log("bom dia")
+        break
+    
+    case "V":
+        console.log("boa tarde")
+        break
+
+    case "N":
+        console.log("Boa noite")
+        break
+
+    default:
+        console.log("Isso não é um turno!")
+        break
+}
+*/
+
+// EXERCÍCIO 7
+
+/*let filme = prompt("Qual o gênero do filme que você vai assistir?")
+let ingresso = Number(prompt("Qual o valor do ingresso?"))
+
+
+if (filme === "fantasia" && ingresso < 15){
+    console.log("Bom Filme!")
+}else{
+    console.log("Escolha outro filme!")
+}
+*/
+
+// DESAFIOS
+
+// DESAFIO 1
+
+/*let filme = prompt("Qual o gênero do filme que você vai assistir?")
+let ingresso = Number(prompt("Qual o valor do ingresso?"))
+
+if (filme === "fantasia" && ingresso < 15){
+    console.log("Bom Filme")
+    let snack = prompt("Escolha o seu snack: chocolate, pipoca ou doces, etc.")
+    console.log("Com " + snack)
+}else{
+    console.log("Escolha outro filme")
+}
+*/
+
+
+
+
+
+
