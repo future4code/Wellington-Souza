d538e3dc8739f8fb108ce960041c7c3937437e5b
Wellington <wellington@192.168.0.2>
2020-10-05 16:12:56 -0300

    Atividade de função

---
+++ semana4/funcoes/src/index.js
@@ -0,0 +1,122 @@
+// EXERCICIO 1 DE INTERPRETAÇÃO DE CÓDIGO
+
+// a. Aparece o resultado 10 e 50
+/* b. Não irá apresentar nada no console, pois ele irá estar retornando as
+funções*/
+
+// EXERCICIO 2 DE INTERPRETAÇÃO DE CÓDIGO
+
+/* 2 a. As saídas impressas no console serão Darvas e Caio, pelo fato da função apenas preencer a array
+somente com dois indece do array */
+
+// 2 b. Amanda e Caio, pois só existiram esses dois itens dentro dess array.
+
+// 3. Ele irá testar a paridade do valor e inserir dentro da arrayFinal. Nome ideal seria testeParidade
+
+// 4 EXERCÍCIO DE ESCRITA DE CÓDIGO
+// 4 a.
+/* function informacoes (){
+    let nome = "Wellington"
+    let idade = 32
+    let cidade = "Rio de Janeiro"
+    let profissao = "estudante"
+
+    console.log("Eu sou " + nome + ", tenho " + idade + " anos, moro no " + cidade + " e sou " + profissao)
+}
+
+const minhasInformacoes = informacoes()
+
+console.log(minhasInformacoes) */
+
+// 4 b. 
+
+/*function minhasInformacoes (nome, idade, endereco, estudante) {
+   
+   if (estudante){
+       estudante = "Sou um estudante"
+   }else{
+       estudante = "Não sou estudante"
+   }
+    console.log ("Eu sou " + nome + ", tenho " + idade + " moro no " + endereco + " e " + estudante)
+    
+}
+
+  minhasInformacoes("Wellington", 32, "Rio de Janeiro", true);*/
+
+
+// 5 EXERCICIO DE ESCRITA
+
+// 5 a.
+
+/*function somaDeDoisNumeros (a, b){
+
+    let resultado = a + b
+
+    return resultado
+    
+}
+
+const resultado1 = somaDeDoisNumeros(25, 32)
+console.log(resultado1)*/
+
+// 5 b.
+
+/*function maiorEigual (a, b){
+    if (a >= b){
+        console.log(true)
+    }else{
+        console.log(false)
+    }
+}
+
+let resultado = maiorEigual(20, 10)
+console.log(resultado)*/
+
+// 5 c. 
+
+/*function receberMensagem (mensagem){
+
+    for (let mensagem1 of mensagem){
+        console.log(mensagem)
+    }
+}
+
+const novaMensagem = recebaUmaMensagem("Olá mundo!")
+console.log(novaMensagem)*/
+
+// 6 EXERCICIO DE ESCRITA DE CÓDIGO
+
+// 6 a.
+
+/*let listarNumeros = (verificaOtamanho) =>{
+
+    for (let elemento of verificaOtamanho){
+        console.log(elemento)
+    }
+
+    return verificaOtamanho.length
+
+} */
+
+// 6 b.
+
+/*function verificarParidade (numero){
+        if(numero % 2 === 0){
+            return true
+        }else {
+            return false
+        }
+}
+
+const numeros = verificarParidade(50)
+console.log(numeros) */
+
+// 6 c.
+
+
+
+
+
+
+
+  
