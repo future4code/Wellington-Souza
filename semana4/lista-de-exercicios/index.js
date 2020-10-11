45959c00edfc0545204963d33ddce01527fc0a01
Wellington de Brito <wellingtondebriito@gmail.com>
2020-10-11 10:18:13 -0300

    resolvido exercicio 3a de funções de array

--- semana4/lista-de-exercicios/index.js
+++ semana4/lista-de-exercicios/index.js
@@ -256,4 +256,25 @@ const arrString = arrayDeNumeros.map ((numero, idx, arrayDeNumeros) =>{
 })
 */
 
+// EXERCICIO 3 :
+
+// a. 
+/*
+const pessoas = [
+	{ nome: "Paula", idade: 12, altura: 1.8},
+	{ nome: "João", idade: 20, altura: 1.3},
+	{ nome: "Pedro", idade: 15, altura: 1.9},
+	{ nome: "Luciano", idade: 22, altura: 1.8},
+	{ nome: "Artur", idade: 10, altura: 1.2},
+	{ nome: "Soter", idade: 70, altura: 1.9}
+]
+
+const altorizaPessoas = pessoas.filter ((pessoa, index, pessoas) =>{
+
+    if (pessoa.idade > 14 && pessoa.altura >= 1.5 && pessoa.idade < 60){
+        return pessoa.nome
+    }
+
+})
+*/
 
